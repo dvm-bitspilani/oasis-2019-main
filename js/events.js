cd9ca5b0e8b4d1bcc0ed5fc6abd9b8291db55adb
mukul0596 <icecoolmkl@gmail.com>
2019-09-10 21:37:33 +0530

    modified events transition

--- js/events.js
+++ js/events.js
@@ -26,105 +26,51 @@ const eventsImgUrl = [
 const eventsContainer = document.getElementsByClassName("events-container")[0];
 
 const numberOfEvents = eventsNames.length;
-let setNumber = 1;
-let numberOfEventsinOneSet;
-if (window.innerWidth < 600) {
-    numberOfEventsinOneSet = 3;
-} else {
-    numberOfEventsinOneSet = 3;
-}
-let numberOfSet = parseInt(numberOfEvents / numberOfEventsinOneSet);
-let numberOfEventsinLastSet = numberOfEvents % numberOfEventsinOneSet;
+let eventIndex = 1;
 
 const createEvent = () => {
-    for (i = 0; i < numberOfSet; i++) {
-        let eventsSet = document.createElement('div');
-        eventsSet.className = "events-set";
-        for (j = 0; j < numberOfEventsinOneSet; j++) {
-            let eventsCard = document.createElement('div');
-            eventsCard.className = "events-card";
-
-            let eventsCardFront = document.createElement('div');
-            eventsCardFront.className = "events-card-front";
-
-            let eventsNameContainer = document.createElement('div');
-            eventsNameContainer.className = "event-name";
-            let eventsName = document.createTextNode(eventsNames[numberOfEventsinOneSet * i + j]);
-            eventsNameContainer.appendChild(eventsName);
-            eventsCardFront.appendChild(eventsNameContainer);
-
-            let eventsCardFrontImg = document.createElement('div');
-            eventsCardFrontImg.className = "event-img"
-            eventsCardFrontImg.setAttribute('style', "background-image: url('" + eventsImgUrl[numberOfEventsinOneSet * i + j] + "')");
-            eventsCardFront.appendChild(eventsCardFrontImg);
-
-            let seperator = document.createElement('hr');
-            seperator.align = 'center';
-            seperator.width = '35%';
-            seperator.size = '5px';
-            seperator.color = '#9A6D9A';
-            eventsCardFront.appendChild(seperator);
-
-            let eventsCardFrontLink = document.createElement('div');
-            eventsCardFrontLink.className = "event-link";
-            eventsCardFrontLink.innerHTML = "View Details >";
-            eventsCardFront.appendChild(eventsCardFrontLink);
-
-            eventsCard.appendChild(eventsCardFront);
-
-            eventsSet.appendChild(eventsCard);
+    for (j = 0; j < numberOfEvents; j++) {
+        let eventsCard = document.createElement('div');
+        eventsCard.className = "events-card";
+        if (j == 1) {
+            eventsCard.classList.add('active'); 
         }
-        eventsContainer.appendChild(eventsSet);
-    }
-    if (numberOfEventsinLastSet != 0) {
-        let eventsSet = document.createElement('div');
-        eventsSet.className = "events-set";
-        eventsSet.style.justifyContent = 'center';
-        for (j = 0; j < numberOfEventsinLastSet; j++) {
-            let eventsCard = document.createElement('div');
-            eventsCard.className = "events-card";
 
-            let eventsCardFront = document.createElement('div');
-            eventsCardFront.className = "events-card-front";
+        let eventsCardFront = document.createElement('div');
+        eventsCardFront.className = "events-card-front";
 
-            let eventsNameContainer = document.createElement('div');
-            eventsNameContainer.className = "event-name";
-            let eventsName = document.createTextNode(eventsNames[numberOfSet * numberOfEventsinOneSet + j]);
-            eventsNameContainer.appendChild(eventsName);
-            eventsCardFront.appendChild(eventsNameContainer);
+        let eventsNameContainer = document.createElement('div');
+        eventsNameContainer.className = "event-name";
+        let eventsName = document.createTextNode(eventsNames[j]);
+        eventsNameContainer.appendChild(eventsName);
+        eventsCardFront.appendChild(eventsNameContainer);
 
-            let eventsCardFrontImg = document.createElement('div');
-            eventsCardFrontImg.className = "event-img";
-            eventsCardFrontImg.setAttribute('style', "background-image: url('" + eventsImgUrl[numberOfSet * numberOfEventsinOneSet + j] + "')");
-            eventsCardFront.appendChild(eventsCardFrontImg);
+        let eventsCardFrontImg = document.createElement('div');
+        eventsCardFrontImg.className = "event-img"
+        eventsCardFrontImg.setAttribute('style', "background-image: url('" + eventsImgUrl[j] + "')");
+        eventsCardFront.appendChild(eventsCardFrontImg);
 
-            // let seperator = document.createElement('div');
-            // seperator.classList = 'seperator';
-            // eventsCardFront.appendChild(seperator);
+        let seperator = document.createElement('hr');
+        seperator.align = 'center';
+        seperator.width = '35%';
+        seperator.size = '5px';
+        seperator.color = '#9A6D9A';
+        eventsCardFront.appendChild(seperator);
 
-            let seperator = document.createElement('hr');
-            seperator.align = 'center';
-            seperator.width = '3px';
-            seperator.size = '20%';
-            seperator.color = '#9A6D9A';
-            eventsCardFront.appendChild(seperator);
+        let eventsCardFrontLink = document.createElement('div');
+        eventsCardFrontLink.className = "event-link";
+        eventsCardFrontLink.innerHTML = "View Details >";
+        eventsCardFront.appendChild(eventsCardFrontLink);
 
-            let eventsCardFrontLink = document.createElement('div');
-            eventsCardFrontLink.className = "event-link";
-            eventsCardFrontLink.innerHTML = "View Details >";
-            eventsCardFront.appendChild(eventsCardFrontLink);
+        eventsCard.appendChild(eventsCardFront);
 
-            eventsCard.appendChild(eventsCardFront);
-
-            eventsSet.appendChild(eventsCard);
-        }
-        eventsContainer.appendChild(eventsSet);
+        eventsContainer.appendChild(eventsCard);
     }
 }
 
 const createEventDots = () => {
     const dotsContainer = document.getElementsByClassName('events-nav-dots')[0];
-    for (i = 0; i < numberOfSet; i++) {
+    for (i = 0; i < (numberOfEvents - 2); i++) {
         let dot = document.createElement('div');
         dot.className = "event-nav-dot";
         dot.id = (i + 1);
@@ -132,13 +78,6 @@ const createEventDots = () => {
         if (i === 0) {
             dot.classList.add('active-dot');
         }
-        dotsContainer.appendChild(dot);
-    }
-    if (numberOfEventsinLastSet != 0) {
-        let dot = document.createElement('div');
-        dot.className = "event-nav-dot";
-        dot.id = (i + 1);
-        dot.setAttribute('onclick', 'navigateEvent(this.id)');
         dotsContainer.appendChild(dot);
     }
 }
@@ -150,34 +89,39 @@ createEvent();
 
 
 const changeEventSet = () => {
-    const limit = (numberOfEventsinLastSet == 0) ? numberOfSet : (numberOfSet + 1);
-    if (setNumber < limit) {
-        setNumber++;
-        eventsContainer.style.transform = "translate(" + (-100 * (setNumber - 1)) + "%)";
+    const limit = numberOfEvents;
+    if (eventIndex < limit) {
+        eventIndex++;
+        eventsContainer.style.transform = "translate(" + (-27 * (eventIndex - 1)) + "%)";
+        document.getElementsByClassName('events-card')[eventIndex].classList.add('active');
         for (dot of document.getElementsByClassName('event-nav-dot')) {
             dot.classList.remove('active-dot');
         }
-        document.querySelectorAll('.events-nav-dots > .event-nav-dot')[setNumber - 1].classList.add('active-dot')
+        document.querySelectorAll('.events-nav-dots > .event-nav-dot')[eventIndex - 1].classList.add('active-dot')
     } else {
-        setNumber = 0;
+        eventIndex = 0;
         changeEventSet();
     }
 }
 
 const navigateEvent = (dotIndex) => {
-    if (dotIndex <= 0 || dotIndex > Math.ceil(numberOfEvents / numberOfEventsinOneSet)) {
-        for (var i = 0; i < document.querySelectorAll('.events-set:nth-of-type(' + setNumber + ') .events-card').length; i++) {
-            document.querySelectorAll('.events-set:nth-of-type(' + setNumber + ') .events-card')[i].style.animation = "shake 0.5s";
+    if (dotIndex <= 0 || dotIndex > (numberOfEvents - 2)) {
+        for (var i = 0; i < document.querySelectorAll('.events-container .events-card').length; i++) {
+            document.querySelectorAll('.events-container .events-card')[i].style.animation = "shake 0.5s";
+            if(i == eventIndex){
+                document.querySelectorAll('.events-container .events-card')[i].style.animation = "shakeEvents 0.5s";
+            }
         }
         setTimeout(() => {
-            for (var i = 0; i < document.querySelectorAll('.events-set:nth-of-type(' + setNumber + ') .events-card').length; i++) {
-                document.querySelectorAll('.events-set:nth-of-type(' + setNumber + ') .events-card')[i].style.animation = "none";
+            for (var i = 0; i < document.querySelectorAll('.events-container .events-card').length; i++) {
+                document.querySelectorAll('.events-container .events-card')[i].style.animation = "none";
             }
         }, 500);
         return;
     }
-    if (setNumber != dotIndex) {
-        setNumber = dotIndex - 1;
+    if (eventIndex != dotIndex) {
+        document.getElementsByClassName('events-card')[eventIndex].classList.remove('active');
+        eventIndex = dotIndex - 1;
         changeEventSet();
     }
 }
